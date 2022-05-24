9a4106ce50342799edd923d5f1ac95d788b489c6
Levent Erguder <erguder.levent@gmail.com>
2022-05-24 08:33:55 +0300

    Chapter01 Destroying Objects

---
+++ OCP17-study-guide/src/main/java/chapter01/Scope.java
@@ -0,0 +1,12 @@
+package chapter01;
+
+public class Scope {
+    public static void main(String[] args) {
+        String one, two;
+        one = new String("a");
+        two = new String("b");
+        one = two;
+        String three = one;
+        one = null;
+    }
+}
