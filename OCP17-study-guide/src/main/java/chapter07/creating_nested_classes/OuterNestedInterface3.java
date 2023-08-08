3c60a9182167524b394a1a1c6ec4cc0ff5c230ac
Levent Erguder <erguder.levent@gmail.com>
2023-08-08 20:58:00 +0300

    Chapter7 review

--- OCP17-study-guide/src/main/java/chapter07/creating_nested_classes/OuterNestedInterface3.java
+++ OCP17-study-guide/src/main/java/chapter07/creating_nested_classes/OuterNestedInterface3.java
@@ -1,13 +1,37 @@
 package chapter07.creating_nested_classes;
 
-public class OuterNestedInterface3 {
+public interface OuterNestedInterface3 {
 
-    class Inner {
-
+    default void defaultMethod() {
+        System.out.println("default method");
     }
 
+    public static void staticMethod() {
+        System.out.println("static method");
+    }
+
+    private static void privateStaticMethod() {
+        System.out.println("private static method");
+    }
+
+    class ImplicitlyStatic {
+
+        void method() {
+            //Non-static method 'defaultMethod()' cannot be referenced from a static context
+            // defaultMethod();
+
+            staticMethod();
+            privateStaticMethod();
+        }
+    }
+
+    // Modifier 'static' is redundant for inner classes of interfaces
     static class StaticNested {
 
+        void method() {
+            staticMethod();
+            privateStaticMethod();
+        }
     }
 
     static enum NestedEnum {
