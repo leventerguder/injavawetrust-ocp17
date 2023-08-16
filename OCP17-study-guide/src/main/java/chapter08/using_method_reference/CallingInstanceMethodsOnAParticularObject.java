978158ff5a7cd089d9ee194c75ff28598ed5eafd
Levent Erguder <erguder.levent@gmail.com>
2023-08-16 07:21:48 +0300

    Chapter8 review

--- OCP17-study-guide/src/main/java/chapter08/using_method_reference/CallingInstanceMethodsOnAParticularObject.java
+++ OCP17-study-guide/src/main/java/chapter08/using_method_reference/CallingInstanceMethodsOnAParticularObject.java
@@ -4,46 +4,126 @@ public class CallingInstanceMethodsOnAParticularObject {
 
     public static void main(String[] args) {
 
+        exampleMethod1StringStart();
+
+        exampleMethod2StringStart();
+
+        exampleMethod3StringStart();
+
+        var callingInstanceMethodsOnAParticularObject = new CallingInstanceMethodsOnAParticularObject();
+        callingInstanceMethodsOnAParticularObject.exampleMethod4StringStart();
+
+        exampleMethod1StringChecker();
+
+        exampleMethod2StringChecker();
+
+        exampleMethod3StringChecker();
+    }
+
+    private static MyClass myClassStaticVariable = new MyClass();
+    private MyClass myClassInstanceVariable = new MyClass();
+
+    private static void exampleMethod1StringStart() {
+        System.out.println("### exampleMethod1StringStart ###");
+
         var str = "Zoo";
         StringStart methodRef = str::startsWith;
         StringStart lambda = s -> str.startsWith(s);
 
+        System.out.println(methodRef.beginningCheck("A"));
+        System.out.println(lambda.beginningCheck("A"));
+    }
+
+    private static void exampleMethod2StringStart() {
+
+        System.out.println("### exampleMethod2StringStart ###");
 
         StringStart stringStart1 = MyClass::staticMethod;
         StringStart stringStart2 = new MyClass()::instanceMethod;
-        MyClass myClass = new MyClass();
-        StringStart stringStart3 = myClass::instanceMethod;
-        StringStart stringStart4 = ss -> myClass.instanceMethod(ss);
+        MyClass myClassRef = new MyClass();
+        StringStart stringStart3 = myClassRef::instanceMethod;
+        // Static method referenced through non-static qualifier
+        //StringStart stringStart4 = myClassRef::staticMethod; // DOES NOT COMPILE
+        StringStart stringStart5 = ss -> myClassRef.instanceMethod(ss);
+        StringStart stringStart6 = ss -> MyClass.staticMethod(ss);
 
         System.out.println(stringStart1.beginningCheck("MyClass"));
         System.out.println(stringStart2.beginningCheck("MyClass"));
         System.out.println(stringStart3.beginningCheck("MyClass"));
-        System.out.println(stringStart4.beginningCheck("MyClass"));
+        System.out.println(stringStart5.beginningCheck("MyClass"));
+        System.out.println(stringStart6.beginningCheck("MyClass"));
+    }
 
-        System.out.println(methodRef.beginningCheck("A"));
-        System.out.println(lambda.beginningCheck("A"));
+    private static void exampleMethod3StringStart() {
 
-        var str2 = "";
-        StringChecker methodRef2 = str2::isEmpty;
-        StringChecker lambda2 = () -> str2.isEmpty();
+        System.out.println("### exampleMethod3StringStart ###");
 
-        System.out.println(methodRef2.check());
-        System.out.println(lambda2.check());
+        StringStart stringStart1 = myClassStaticVariable::instanceMethod;
+        // StringStart stringStart2 = myClassInstanceVariable::instanceMethod; // DOES NOT COMPILE
+
+        //Static method referenced through non-static qualifier
+        // StringStart stringStart3 = myClassStaticVariable::staticMethod; // DOES NOT COMPILE
+
+        System.out.println(stringStart1.beginningCheck("MyClass"));
+    }
+
+    private void exampleMethod4StringStart() {
+
+        System.out.println("### exampleMethod4StringStart ###");
+
+        StringStart stringStart1 = myClassStaticVariable::instanceMethod;
+        StringStart stringStart2 = myClassInstanceVariable::instanceMethod;
+
+        // Static method referenced through non-static qualifier
+        // StringStart stringStart3 = myClassStaticVariable::staticMethod; // DOES NOT COMPILE
+        // StringStart stringStart4 = myClassStaticVariable::staticMethod; // DOES NOT COMPILE
+
+        StringStart stringStart5 = MyClass::staticMethod;
+
+        System.out.println(stringStart1.beginningCheck("MyClass"));
+        System.out.println(stringStart2.beginningCheck("MyClass"));
+        System.out.println(stringStart5.beginningCheck("MyClass"));
+    }
+
+    private static void exampleMethod1StringChecker() {
+
+        System.out.println("### exampleMethod1StringChecker ###");
+
+        var str = "";
+        StringChecker methodRef = str::isEmpty;
+        StringChecker lambda = () -> str.isEmpty();
+
+        System.out.println(methodRef.check());
+        System.out.println(lambda.check());
+    }
+
+    private static void exampleMethod2StringChecker() {
+
+        System.out.println("### exampleMethod2StringChecker ###");
 
         MyClassEmptyCheck myClassEmptyCheck = new MyClassEmptyCheck();
 
         StringChecker stringChecker1 = myClassEmptyCheck::instanceMethod;
         StringChecker stringChecker2 = MyClassEmptyCheck::staticMethod;
 
-
-        var str3 = "";
-        StringChecker lambda3 = () -> str.startsWith("Zoo");
-
-        System.out.println(lambda3.check());
-
-        // StringChecker methodReference3 = str::startsWith; // DOES NOT COMPILE
-        // StringChecker methodReference3 = str::startsWith ("Zoo"); // DOES NOT COMPILE
+        System.out.println(stringChecker1.check());
+        System.out.println(stringChecker2.check());
     }
+
+    private static void exampleMethod3StringChecker() {
+
+        var str = "";
+        StringChecker lambda = () -> str.startsWith("Zoo");
+        //StringChecker lambda2 = s -> str.startsWith("Zoo"); // DOES NOT COMPILE
+        //StringChecker lambda3 = (String s) -> str.startsWith("Zoo"); // DOES NOT COMPILE
+
+        System.out.println(lambda.check());
+
+        // StringChecker methodReference = str::startsWith; // DOES NOT COMPILE
+        // StringChecker methodReference = str::startsWith ("Zoo"); // DOES NOT COMPILE
+    }
+
+
 }
 
 
--- OCP17-study-guide/src/main/java/chapter08/using_method_reference/CallingStaticMethods.java
+++ OCP17-study-guide/src/main/java/chapter08/using_method_reference/CallingStaticMethods.java
@@ -6,6 +6,7 @@ public class CallingStaticMethods {
 
         Converter methodRef = Math::round;
         Converter lambda = x -> Math.round(x);
+        Converter lambda2 = (double x) -> Math.round(x);
         Converter methodRef2 = StaticMethodRef::deferred;
 
         System.out.println(methodRef.round(101.1));
@@ -13,10 +14,29 @@ public class CallingStaticMethods {
         System.out.println(methodRef2.round(101.1));
 
         GreetingInterface greetingInterfaceMethodRef = StaticMethodRef::printMessage;
+
         GreetingInterface greetingInterfaceLamda = () -> StaticMethodRef.printMessage();
+
+        greetingInterfaceMethodRef.message();
+        greetingInterfaceLamda.message();
 
         // GreetingInterface greetingInterfaceLamda2 =  -> StaticMethodRef.printMessage(); // DOES NOT COMPILE
         // GreetingInterface greetingInterfaceLamda3 =  x -> StaticMethodRef.printMessage(); // DOES NOT COMPILE
+
+        GreetingInterface greetingInterfaceLambda4 = () -> StaticMethodRef.printMessageV2();
+        GreetingInterface greetingInterfaceLambda5 = () -> { StaticMethodRef.printMessageV2(); };
+        // GreetingInterface greetingInterfaceLambda6 = () -> { return StaticMethodRef.printMessageV2(); }; // DOES NOT COMPILE
+        GreetingInterface greetingInterfaceMethodRef2 = StaticMethodRef::printMessageV2;
+
+        GreetingInterface greetingInterfaceLambda7 = () -> StaticMethodRef.printMessageV3("hello printMessageV3");
+        // GreetingInterface greetingInterfaceMethodRef3 = StaticMethodRef::printMessageV3; // DOES NOT COMPILE
+
+        greetingInterfaceMethodRef2.message();
+        greetingInterfaceLambda4.message();
+        greetingInterfaceLambda5.message();
+        greetingInterfaceLambda7.message();
+
+
     }
 
 }
@@ -29,5 +49,15 @@ class StaticMethodRef {
 
     static void printMessage() {
         System.out.println("hello printMessage!");
+    }
+
+    static String printMessageV2() {
+        String message = "hello printMessageV2";
+        System.out.println(message);
+        return message;
+    }
+
+    static void printMessageV3(String message) {
+        System.out.println(message);
     }
 }
