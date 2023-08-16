a2f4196a8a9985c11da0fa24d315f9e5cc428e4f
Levent Erguder <erguder.levent@gmail.com>
2023-08-16 22:07:14 +0300

    Chapter8 review

--- OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/ImplementingConsumerAndBiConsumer.java
+++ OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/ImplementingConsumerAndBiConsumer.java
@@ -1,6 +1,8 @@
 package chapter08.working_with_built_in_functional_interfaces;
 
+import java.io.PrintStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -8,25 +10,68 @@ public class ImplementingConsumerAndBiConsumer {
 
     public static void main(String[] args) {
 
-        consumerTest();
-        biConsumerTest();
-        biConsumerTest2();
+        consumerExample();
+        consumerHelperExample();
+        biConsumerExample1();
+        biConsumerHelperExample();
+        biConsumerExample2();
+        biConsumerExample3();
+
+
     }
 
-    private static void consumerTest() {
+    private static void consumerExample() {
 
+        System.out.println("### consumerExample ###");
         Consumer<String> c1 = System.out::println;
         Consumer<String> c2 = x -> System.out.println(x);
 
+        PrintStream ps = System.out;
+        Consumer<String> c3 = ps::println;
+
+
+        Consumer<StringBuilder> c4 = System.out::println;
+        Consumer<List> c5 = System.out::println;
+
         c1.accept("Annie");
         c2.accept("Annie");
+        c3.accept("Annie");
+        c4.accept(new StringBuilder("Annie"));
+        c5.accept(List.of("Annie"));
 
     }
 
-    private static void biConsumerTest() {
+    private static void consumerHelperExample() {
 
+        System.out.println("### consumerHelperExample ###");
+        Consumer<String> c1 = ConsumerHelper::method;
+        Consumer<Integer> c2 = ConsumerHelper::method;
+        Consumer<Integer> c3 = ConsumerHelper::methodWithReturn;
+
+        Consumer<String> c4 = s -> ConsumerHelper.method(s);
+        // Consumer<String> c5 = s -> "value"; // DOES NOT COMPILE
+
+        Consumer<String> c6 = s -> {
+            System.out.println(s);
+            System.out.println(s.length());
+        };
+
+        Consumer<StringBuilder> c7 = ConsumerHelper::method;
+        Consumer<List<String>> c8 = ConsumerHelper::method;
+
+        c1.accept("hello world!");
+        c2.accept(10);
+        c3.accept(20);
+        c4.accept("hello world!");
+        c6.accept("hello world!");
+        c7.accept(new StringBuilder("hello world!"));
+        c8.accept(List.of("v1", "v2", "v3"));
+    }
+
+    private static void biConsumerExample1() {
+
+        System.out.println("### biConsumerExample1 ###");
         var map = new HashMap<String, Integer>();
-
         BiConsumer<String, Integer> b1 = map::put;
         BiConsumer<String, Integer> b2 = (k, v) -> map.put(k, v);
 
@@ -36,10 +81,39 @@ public class ImplementingConsumerAndBiConsumer {
         System.out.println(map);
     }
 
-    private static void biConsumerTest2() {
+    private static void biConsumerHelperExample() {
 
+        System.out.println("### biConsumerHelperExample ###");
+        BiConsumer<Integer, Integer> bc1 = BiConsumerHelper::method;
+        BiConsumer<String, Integer> bc2 = BiConsumerHelper::method;
+
+        bc1.accept(5, 10);
+        bc2.accept("10", 20);
+    }
+
+    private static void biConsumerExample2() {
+
+        System.out.println("### biConsumerExample2 ###");
+        BiConsumer<String, String> bc1 = String::concat;
+        BiConsumer<String, String> bc2 = (str1, str2) -> str1.concat(str2);
+        BiConsumer<String, String> bc3 = (str1, str2) ->
+        {
+            System.out.println(str1.concat(str2));
+        };
+
+        BiConsumer<BiConsumerHelper, String> bc4 = BiConsumerHelper::instanceMethod;
+
+
+        bc1.accept("hello", "world");
+        bc2.accept("hello", "world");
+        bc3.accept("hello", "world");
+        bc4.accept(new BiConsumerHelper(), "message!");
+    }
+
+    private static void biConsumerExample3() {
+
+        System.out.println("### biConsumerExample3 ###");
         var map = new HashMap<String, String>();
-
         BiConsumer<String, String> b1 = map::put;
         BiConsumer<String, String> b2 = (k, v) -> map.put(k, v);
 
@@ -48,4 +122,44 @@ public class ImplementingConsumerAndBiConsumer {
 
         System.out.println(map);
     }
+}
+
+class ConsumerHelper {
+
+    static void method(String str) {
+        System.out.println(str);
+    }
+
+    static void method(Integer i) {
+        System.out.println(i);
+    }
+
+    static int methodWithReturn(Integer i) {
+        System.out.println(i * 2);
+        return i * 2;
+    }
+
+    static void method(StringBuilder sb) {
+        System.out.println(sb);
+    }
+
+    static void method(List<String> list) {
+        System.out.println(list);
+    }
+}
+
+class BiConsumerHelper {
+
+    static void method(Integer i, Integer j) {
+        System.out.println(i + j);
+    }
+
+    static void method(String str, Integer j) {
+        System.out.println(Integer.parseInt(str) * j);
+    }
+
+    void instanceMethod(String str) {
+        System.out.println(str);
+    }
+
 }
--- OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/ImplementingSupplier.java
+++ OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/ImplementingSupplier.java
@@ -2,20 +2,24 @@ package chapter08.working_with_built_in_functional_interfaces;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.function.Supplier;
 
 public class ImplementingSupplier {
 
     public static void main(String[] args) {
 
-        supplierTest1();
-        supplierTest2();
-        supplierTest3();
-        supplierTest4();
+        localDateSupplierExample();
+        stringBuilderSupplierExample();
+        arrayListSupplierExample();
+        randomSupplierExample();
+        stringSupplierExample();
+        supplierHelperExample();
     }
 
-    private static void supplierTest1() {
+    private static void localDateSupplierExample() {
 
+        System.out.println("### localDateSupplierExample ###");
         Supplier<LocalDate> s1 = LocalDate::now;
         Supplier<LocalDate> s2 = () -> LocalDate.now();
 
@@ -26,8 +30,9 @@ public class ImplementingSupplier {
         System.out.println(d2);
     }
 
-    private static void supplierTest2() {
+    private static void stringBuilderSupplierExample() {
 
+        System.out.println("### stringBuilderSupplierExample ###");
         Supplier<StringBuilder> s1 = StringBuilder::new;
         Supplier<StringBuilder> s2 = () -> new StringBuilder();
 
@@ -36,18 +41,99 @@ public class ImplementingSupplier {
 
     }
 
-    private static void supplierTest3() {
-        Supplier<ArrayList<String>> s3 = ArrayList::new;
-        ArrayList<String> a1 = s3.get();
+    private static void arrayListSupplierExample() {
+
+        System.out.println("### arrayListSupplierExample ###");
+        Supplier<ArrayList<String>> s1 = ArrayList::new;
+        ArrayList<String> a1 = s1.get();
         System.out.println(a1); // []
 
-        System.out.println(s3);
+        System.out.println(s1);
 
     }
 
-    private static void supplierTest4() {
+    private static void randomSupplierExample() {
 
+        System.out.println("### randomSupplierExample ###");
         Supplier<Double> s1 = () -> Math.random();
+        Supplier<Double> s2 = Math::random;
+        Supplier<Integer> s3 = new Random()::nextInt;
+
         System.out.println(s1.get());
+        System.out.println(s2.get());
+        System.out.println(s3.get());
+    }
+
+    private static void stringSupplierExample() {
+
+        System.out.println("### stringSupplierExample ###");
+        Supplier<String> s1 = String::new;
+        Supplier<String> s2 = () -> "value";
+
+        // Supplier<Integer> s3 = Integer::new; // DOES NOT COMPILE
+        Supplier<Integer> s4 = () -> 100;
+        Supplier<Integer> s5 = () -> Integer.valueOf(100);
+
+
+        System.out.println(s1.get());
+        System.out.println(s2.get());
+        System.out.println(s4.get());
+        System.out.println(s5.get());
+    }
+
+
+    private static void supplierHelperExample() {
+
+        System.out.println("### supplierHelperExample ###");
+        Supplier<Integer> s1 = SupplierHelper::method1;
+        Supplier<Integer> s2 = () -> SupplierHelper.method1();
+        Supplier<Integer> s3 = () -> {
+            return SupplierHelper.method1();
+        };
+        Supplier<Integer> s4 = () -> 10;
+
+        Supplier<Integer> s5 = SupplierHelper::method2;
+
+        // Bad return type in method reference: cannot convert long to java.lang.Integer
+        // Supplier<Integer> s6 = SupplierHelper::method3; // DOES NOT COMPILE
+
+        // ad return type in method reference: cannot convert short to java.lang.Integer
+        // Supplier<Integer> s7 = SupplierHelper::method4; // DOES NOT COMPILE
+
+        // Supplier<Integer> s8 = SupplierHelper::instanceMethod; // DOES NOT COMPILE
+
+        Supplier<Integer> s9 = new SupplierHelper()::instanceMethod;
+
+        System.out.println(s1.get());
+        System.out.println(s2.get());
+        System.out.println(s3.get());
+        System.out.println(s4.get());
+        System.out.println(s5.get());
+        System.out.println(s9.get());
+
+    }
+
+}
+
+class SupplierHelper {
+
+    static int method1() {
+        return new Random().nextInt();
+    }
+
+    static Integer method2() {
+        return new Random().nextInt();
+    }
+
+    static long method3() {
+        return new Random().nextInt();
+    }
+
+    static short method4() {
+        return (short) new Random().nextInt();
+    }
+
+    int instanceMethod() {
+        return new Random().nextInt();
     }
 }
