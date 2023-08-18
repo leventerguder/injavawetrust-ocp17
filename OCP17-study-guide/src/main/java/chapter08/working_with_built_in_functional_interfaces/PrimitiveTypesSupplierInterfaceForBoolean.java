e57bc73fb7a6011637e03513dfdb64e1e82f2734
Levent Erguder <erguder.levent@gmail.com>
2023-08-18 08:08:50 +0300

    Chapter8 review

--- OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/PrimitiveTypesSupplierInterfaceForBoolean.java
+++ OCP17-study-guide/src/main/java/chapter08/working_with_built_in_functional_interfaces/PrimitiveTypesSupplierInterfaceForBoolean.java
@@ -1,5 +1,6 @@
 package chapter08.working_with_built_in_functional_interfaces;
 
+import java.util.Random;
 import java.util.function.BooleanSupplier;
 
 public class PrimitiveTypesSupplierInterfaceForBoolean {
@@ -8,9 +9,22 @@ public class PrimitiveTypesSupplierInterfaceForBoolean {
 
         BooleanSupplier b1 = () -> true;
         BooleanSupplier b2 = () -> Math.random() > .5;
+        BooleanSupplier b3 = PrimitiveBooleanHelper::isRandomNumGreaterThan5;
+
+        String str = "not-empty";
+        BooleanSupplier b4 = str::isEmpty;
 
         System.out.println(b1.getAsBoolean());
         System.out.println(b2.getAsBoolean());
+        System.out.println(b3.getAsBoolean());
+        System.out.println(b4.getAsBoolean());
 
+    }
+}
+
+class PrimitiveBooleanHelper {
+
+    static boolean isRandomNumGreaterThan5() {
+        return new Random().nextInt(10) > 5;
     }
 }
