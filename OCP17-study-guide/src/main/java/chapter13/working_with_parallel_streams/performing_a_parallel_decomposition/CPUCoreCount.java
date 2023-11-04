76a3a19d3c97e20ad4f5981f97c20584025d6cc0
Levent Erguder <erguder.levent@gmail.com>
2023-11-04 12:32:39 +0300

    Chapter13 review

--- OCP17-study-guide/src/main/java/chapter13/working_with_parallel_streams/performing_a_parallel_decomposition/CPUCoreCount.java
+++ OCP17-study-guide/src/main/java/chapter13/working_with_parallel_streams/performing_a_parallel_decomposition/CPUCoreCount.java
@@ -4,6 +4,7 @@ public class CPUCoreCount {
 
     public static void main(String[] args) {
 
+        // Returns the number of processors available to the Java virtual machine.
         int cores = Runtime.getRuntime().availableProcessors();
 
         System.out.println(cores);
