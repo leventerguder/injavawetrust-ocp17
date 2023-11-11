a4da7555acd19ab9c86ced9b3f2ae8f3378cacaf
Levent Erguder <erguder.levent@gmail.com>
2023-11-11 19:22:48 +0300

    Chapter14 review

--- OCP17-study-guide/src/main/java/chapter14/operating_on_file_and_path/providing_nio2_optional_parameters/LinkOptionNoFollowLinks.java
+++ OCP17-study-guide/src/main/java/chapter14/operating_on_file_and_path/providing_nio2_optional_parameters/LinkOptionNoFollowLinks.java
@@ -1,5 +1,6 @@
 package chapter14.operating_on_file_and_path.providing_nio2_optional_parameters;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -7,10 +8,17 @@ import java.nio.file.Paths;
 
 public class LinkOptionNoFollowLinks {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("schedule.xml");
-        boolean exists = Files.exists(path, LinkOption.NOFOLLOW_LINKS);
-        System.out.println(exists);
+        Path newLink = Paths.get("schedule.xml");
+        Path target = Path.of("another-schedule.xml");
+
+        //Files.createFile(target);
+
+        Files.createSymbolicLink(newLink, target);
+        System.out.println("Files.exists: " + Files.exists(newLink));
+        System.out.println("isSymbolicLink :" + Files.isSymbolicLink(newLink));
+        System.out.println("Files.exists#NOFOLLOW_LINKS : " + Files.exists(newLink, LinkOption.NOFOLLOW_LINKS));
+        System.out.format("Target of link" + " '%s' is '%s'%n", newLink, Files.readSymbolicLink(newLink));
     }
 }
