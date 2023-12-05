7ecd0332b3cf36b4f388779514a8d53b8b9704bb
Levent Erguder <erguder.levent@gmail.com>
2023-12-05 21:03:25 +0300

    Chapter15 review

--- OCP17-study-guide/src/main/java/chapter15/calling_a_callablestatement/ReturningAnOutParameter.java
+++ OCP17-study-guide/src/main/java/chapter15/calling_a_callablestatement/ReturningAnOutParameter.java
@@ -1,21 +1,29 @@
 package chapter15.calling_a_callablestatement;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Types;
 
 public class ReturningAnOutParameter {
 
-    Connection conn = null;
+    public static void main(String[] args) throws SQLException {
 
-    void test() throws SQLException {
+        test();
+    }
+
+    static void test() throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:hsqldb:file:zoo");
 
         var sql = "{?= call magic_number(?) }";
 
         try (var cs = conn.prepareCall(sql)) {
             cs.registerOutParameter(1, Types.INTEGER);
             cs.execute();
+            //cs.executeQuery();  // java.sql.SQLException: statement does not generate a result set
             System.out.println(cs.getInt("num"));
+            System.out.println(cs.getInt(1));
         }
     }
 }
--- OCP17-study-guide/src/main/java/chapter15/calling_a_callablestatement/WorkingWithAnInOutParameter.java
+++ OCP17-study-guide/src/main/java/chapter15/calling_a_callablestatement/WorkingWithAnInOutParameter.java
@@ -1,14 +1,19 @@
 package chapter15.calling_a_callablestatement;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Types;
 
 public class WorkingWithAnInOutParameter {
 
-    Connection conn = null;
+    public static void main(String[] args) throws SQLException {
+        test();
+    }
+    static void test() throws SQLException {
 
-    void test() throws SQLException {
+
+        Connection conn = DriverManager.getConnection("jdbc:hsqldb:file:zoo");
 
         var sql = "{call double_number(?)}";
         try (var cs = conn.prepareCall(sql)) {
@@ -16,6 +21,7 @@ public class WorkingWithAnInOutParameter {
             cs.registerOutParameter(1, Types.INTEGER);
             cs.execute();
             System.out.println(cs.getInt("num"));
+            System.out.println(cs.getInt(1));
         }
     }
 }
