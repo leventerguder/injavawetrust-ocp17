3899150f19cc8cf434abe4f78d4f907901fe5dc1
Levent Erguder <erguder.levent@gmail.com>
2024-06-29 13:24:32 +0300

    OCP11-Chapter13-Java annotations revisit

---
+++ OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example03/RetentionPolicies.java
@@ -0,0 +1,36 @@
+package chapter13.declaring_annotation_specific_annotations.example03;
+
+import java.lang.annotation.Annotation;
+
+public class RetentionPolicies {
+
+    public static void main(String[] args) {
+        Annotation a[]
+                = new chapter13.declaring_annotation_specific_annotations.example03.A().getClass().getAnnotations();
+        Annotation b[]
+                = new chapter13.declaring_annotation_specific_annotations.example03.B().getClass().getAnnotations();
+        Annotation c[]
+                = new chapter13.declaring_annotation_specific_annotations.example03.C().getClass().getAnnotations();
+
+
+        System.out.println("Number of annotations attached to " + "class A at Runtime: " + a.length);
+
+        System.out.println("Number of annotations attached to " + "class B at Runtime: " + b.length);
+
+        System.out.println("Number of annotations attached to " + "class C at Runtime: " + c.length);
+
+        System.out.println("Annotation attached to class C: " + c[0]);
+    }
+}
+
+@Flier
+class A {
+}
+
+@Walker
+class B {
+}
+
+@Swimmer
+class C {
+};
--- OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example03/Walker.java
+++ OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example03/Walker.java
@@ -3,6 +3,6 @@ package chapter13.declaring_annotation_specific_annotations.example03;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(RetentionPolicy.CLASS)
-public @interface Flier {
+@Retention(RetentionPolicy.SOURCE)
+public @interface Walker {
 }
