d0eaf5f300aba29db7346444b389d12e8b4365e1
Levent Erguder <erguder.levent@gmail.com>
2024-06-29 12:31:31 +0300

    OCP11-Chapter13-Java annotations revisit

--- OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example02/NetworkRepair.java
+++ OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example02/NetworkRepair.java
@@ -6,11 +6,32 @@ public class NetworkRepair {
     class OutSrc extends @Technical NetworkRepair {
     }
 
+    // @Technical // DOES NOT COMPILE
+    @TechnicalMethod
     public void repair() {
         var repairSubclass = new @Technical NetworkRepair() {
         };
         var o = new @Technical NetworkRepair().new @Technical
                 OutSrc();
         int remaining = (@Technical int) 10.0;
+    }
+
+    @Technical
+    @TechnicalMethod
+    // @TechnicalConstructor // DOES NOT COMPILE
+    public int value() {
+        return 10;
+    }
+
+    @Technical
+    // @TechnicalMethod // DOES NOT COMPILE
+    // @TechnicalConstructor // DOES NOT COMPILE
+    private int size = 100;
+
+    @Technical
+    @TechnicalConstructor
+    // @TechnicalMethod // DOES NOT COMPILE
+    public NetworkRepair() {
+
     }
 }
---
+++ OCP11-bonus/src/main/java/chapter13/declaring_annotation_specific_annotations/example02/TechnicalConstructor.java
@@ -0,0 +1,8 @@
+package chapter13.declaring_annotation_specific_annotations.example02;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.CONSTRUCTOR)
+public @interface TechnicalConstructor {
+}
